dc5b7f39bdaacabc6ef9c9dce23fd30f0c61516e
jescarcon <jesuscardenas2000@gmail.com>
2024-05-26 22:01:46 +0200

    QuickFix

--- src/Deploy/build.sh
+++ src/Deploy/build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
+cd .. #Default route at BackEnd
+cd /Deploy #Navigate Deploy
+
 pip install -r requirements.txt
+
+cd .. #Default route at BackEnd
+cd /BackEnd #Continue at BackEnd
 
 python manage.py collectstatic --no-input
 python manage.py migrate
