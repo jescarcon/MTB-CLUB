c9fb25c16437614731ea2b70dd9d865f41e78233
jescarcon <jesuscardenas2000@gmail.com>
2024-05-26 21:40:30 +0200

    navigating fix

--- src/Deploy/build.sh
+++ src/Deploy/build.sh
@@ -6,8 +6,8 @@ cd src/Deploy #navigate to deploy for requirements
 
 pip install -r requirements.txt
 
-cd.. #back to src, getting out of deploy
-cd BackEnd
+cd .. #back to src, getting out of deploy
+cd BackEnd 
 
 python manage.py collectstatic --no-input
 python manage.py migrate
