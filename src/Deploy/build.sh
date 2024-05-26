48419dfcd701048dbf261623520b3b68f51590ea
jescarcon <jesuscardenas2000@gmail.com>
2024-05-26 21:38:45 +0200

    Ajuste ruta manage.py

--- src/Deploy/build.sh
+++ src/Deploy/build.sh
@@ -2,9 +2,12 @@
 # exit on error
 set -o errexit
 
-cd src/Deploy
+cd src/Deploy #navigate to deploy for requirements
 
 pip install -r requirements.txt
+
+cd.. #back to src, getting out of deploy
+cd BackEnd
 
 python manage.py collectstatic --no-input
 python manage.py migrate
