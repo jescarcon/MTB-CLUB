bb48beba9ae7502e119c42275168c1a57d917336
jescarcon <jesuscardenas2000@gmail.com>
2024-05-26 21:51:43 +0200

    Fixed Deploy

--- build.sh
+++ build.sh
@@ -2,12 +2,7 @@
 # exit on error
 set -o errexit
 
-cd src/Deploy #navigate to deploy for requirements
-
 pip install -r requirements.txt
-
-cd .. #back to src, getting out of deploy
-cd BackEnd 
 
 python manage.py collectstatic --no-input
 python manage.py migrate
